6deab22c0a57adb090febfac2552737763da4adb
Bibekpandey <spirit_bibek@yahoo.com>
2017-11-02 21:32:25 +0000

    solve problem 112

--- euler112.py
+++ euler112.py
@@ -1,36 +1,46 @@
-# wrong!!
+# function is_bouncy is better than check_bouncy
 bouncy = 0
 
 def check_bouncy(n):
-    inc = 0
-    dec = 0
-    a = n%10
-    n = n//10
-    b = n%10
-    n = n//10
-    while n!=0:
-        if a>b:
-            dec = 1
-        if a<b:
-            inc = 1
-        if dec and inc:
-            return True
-        a = b
-        b = n%10
-        n = n//10
+    inc=False
+    dec=False
+    n = str(n)
+    # prev = n%10
+    for i in range(len(n)-1):
+        if n[i] == n[i+1]:continue
+        if n[i] > n[i+1]:
+            dec=True
+        else: inc=True
+        if dec and inc:return True
     return False
 
-# number of bouncy for n digits
-n = 2
-incr = 0
-decr = 0
-total = 10**n-1
+def is_bouncy(n):
+    inc=False
+    dec=False
+    prev = n%10
+    n //= 10
+    while n>0:
+        curr = n%10
+        if prev==curr:
+            n //= 10
+            continue
+        elif prev>curr:inc=True
+        else:dec=True
+        if inc and dec:
+            return True
+        prev=curr
+        n//=10
+    return False
 
-for s in range(1,10):
-    for e in range(1,10):
-        incr+=(int(e-s+1)**n-2)
-decr = incr
-decr += 9*(n-1)
+print(is_bouncy(66420))
 
-bouncy = total - incr-decr
-print(bouncy, total)
+percent = 99
+
+n = 100
+while True:
+    if is_bouncy(n):
+        bouncy+=1
+    if 100*bouncy == 99*n:
+        print(n)
+        break
+    n+=1
